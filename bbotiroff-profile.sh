bda082291ea44ea1a5bf4a7c53a831d27b1bd10c
Bakhrom Botirov <bakhrom.botiroff@gmail.com>
2019-10-27 23:06:03 -0700

    Parsing git branch

--- bbotiroff-profile.sh
+++ bbotiroff-profile.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\w/\[\033[33;1m\]\$(parse_git_branch)\[\033[00m\]\[\033[m\]\\n$ "
+export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\w/\[\033[33;1m\]\$(parse_current_branch)\[\033[00m\]\[\033[m\]\\n$ "
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
 
-parse_git_branch() {
+parse_current_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 
 gpush() {
-  git push --set-upstream origin `git branch --show-current`
+  git push --set-upstream origin $(git branch | grep \* | cut -d ' ' -f2)
 }
 
 alias ll='ls -la'
