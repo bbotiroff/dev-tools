feb67247710101cd08e108469f20d224f49773c9
Bakhrom  Botirov <bakhrom.botiroff@gmail.com>
2026-06-03 17:28:12 -0700

    Adding git remove all the branch shortcut

--- git-shortcuts.sh
+++ git-shortcuts.sh
@@ -47,6 +47,30 @@ gdeletebranches() {
     git branch | grep -v ^* |  grep -v "develop" | grep -v "dev" | grep -v "staging" | grep -v "master" | grep -v "main" | xargs git branch -D
 }
 
+# Delete ALL local branches except main, master, and the current branch (asks to confirm)
+gbclean() {
+  local targets
+  targets=$(git branch | grep -v '^[*+]' | sed 's/^ *//' | grep -vxE 'main|master')
+
+  if [ -z "$targets" ]; then
+    echo "No branches to delete."
+    return 0
+  fi
+
+  echo "The following local branches will be force-deleted:"
+  echo "$targets" | sed 's/^/  /'
+  printf "Proceed? (y/N) "
+  read -r reply
+  case "$reply" in
+    [yY]|[yY][eE][sS])
+      echo "$targets" | xargs git branch -D
+      ;;
+    *)
+      echo "Aborted."
+      ;;
+  esac
+}
+
 # starts the maintenance on the repository
 gmmaintenance() {
   git maintenance start
